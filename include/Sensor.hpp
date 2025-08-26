3a3510d5e58874624fcc7869f14d6d7ecbb4b969
OZiad <o.ziad@outlook.com>
2025-08-25 21:31:45 -0400

    feat(sensor): add Sensor.hpp interface

---
+++ include/Sensor.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "ThreadSafeQueue.hpp"
+
+
+class Sensor
+{
+public:
+    explicit Sensor(ThreadSafeQueue<int> outQue, int hz = 10);
+    ~Sensor();
+    void start();
+    void stop();
+
+private:
+    void run();
+    ThreadSafeQueue<int>& _out;
+    int _periodMs;
+    std::atomic<bool> _running{false};
+    std::thread _worker;
+};
